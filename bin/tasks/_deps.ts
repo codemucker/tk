e49df256885f5d09303af9737d50d02947a66847
Bert van Brakel <bertvanbrakel@gmail.com>
2020-09-22 22:13:44 +1000

    remove pointless exports

--- bin/tasks/_deps.ts
+++ bin/tasks/_deps.ts
@@ -1,11 +1,4 @@
 export { existsSync } from "https://deno.land/std/fs/mod.ts";
-export {
-    exec,
-    getLogger,
-    getTaskContext,
-    run,
-    TaskContext,
-} from "https://raw.githubusercontent.com/codemucker/deno-script/master/mod.ts";
+export { exec, getLogger } from "https://raw.githubusercontent.com/codemucker/deno-script/master/mod.ts";
 export { path };
-
 import * as path from "https://deno.land/std/path/mod.ts";

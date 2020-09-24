ebfd12b8b8c88f5f875fcce3abeb599f1b53aec4
Bert van Brakel <bertvanbrakel@gmail.com>
2020-09-24 12:10:59 +1000

    improve nstall script

--- install.sh
+++ install.sh
@@ -4,16 +4,6 @@
 # Install the tk (TookKit) runner
 #
 
-set -Eeuo pipefail & set -e
-
-cd "${0%/*}/.." # set the CWD to the root dir
-
-if ! command -v tk command -v o &> /dev/null; then
-    echo "'tk' is not installed. Installing to ~/.tk" 
-    git clone git@github.com:codemucker/tk.git ~/.tk/
-    pushd ~/.tk/ >/dev/null
-        git checkout master
-        ln -s ~/.tk/bin/tk ~/bin/tk
-    popd >/dev/null
-fi
-
+set -Eeuo pipefail
+git clone git@github.com:codemucker/tk.git -branch master --single-branch  ~/.tk/
+ln -s ~/.tk/bin/tk ~/bin/tk

05e56898133b1e2a41aab7ff1413a8e4141f2f78
Bert van Brakel <bertvanbrakel@gmail.com>
2020-09-24 12:22:33 +1000

    add message of existing tk

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-
 #
 # Install the tk (TookKit) runner
 #
-
 set -Eeuo pipefail
 tk_home="$HOME/.tk"
-
 if [[ -d "$tk_home" ]]; then
 	echo "'tk' already installed in '$tk_home'. Run 'tk tk.update' to upgrade to latest version"
 else
 	echo "installing tk to '$tk_home'"
 	git clone --branch master --single-branch  git@github.com:codemucker/tk.git "$tk_home"
 fi
-if [[ ! -f "$HOME/bin/tk" ]]; then
+if [[ -f "$HOME/bin/tk" ]]; then
+	echo "'$HOME/bin/tk' already exists, doing nothing"
+else
 	ln -s "$tk_home/bin/tk" "$HOME/bin/tk"
 fi
